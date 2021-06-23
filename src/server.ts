d9803312f50f806b19a185185c6e5bd941bfa3c1
victorpremoli <victor.premoli57@gmail.com>
2021-06-22 21:51:04 -0300

    creating exception handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express, { request, response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors"
 import { router } from "./routes"
 
 
@@ -9,5 +10,19 @@ const app = express();
 
 app.use(express.json()) // talking to express that i will work with JSON
 app.use(router);
+
+//mid
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => console.log('server is running'));
